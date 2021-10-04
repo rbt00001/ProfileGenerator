7e856367a8472c035c3a34b749d00fd1d6c77900
Ben Townsend <RBT000011@gmail.com>
2021-10-04 18:30:06 -0500

    psudo code addition

---
+++ _tests_/employee.test.js
@@ -0,0 +1,6 @@
+// require employee folder
+// employee class 
+
+// does getRole() method return role "Employee"
+
+// when I create a new employee object, does this have a name, email, and ID that match created?
---
+++ index.js
@@ -0,0 +1,8 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+
+// Start - Create manager. Enter 4 manager prompts = Then Enter Engineer, Employee, or Finished List
+// "Would you like to enter a new employee?"(yes/no)
+//If yes - Go to list. - List shows Employee / Engineer / Intern / Manager 
+// From the selected agent follow along with prompts to enter all of the ID information. 
