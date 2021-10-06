36f5b48cec2918a3b0b1fb800d1e985bfe598522
Ben Townsend <RBT000011@gmail.com>
2021-10-06 17:46:23 -0500

    added JS to create employee

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
+// node modules 
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+// lib modules 
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern.js");
+const Manager = require("./lib/manager");
+
+
+
+
 
 // Start - Create manager. Enter 4 manager prompts = Then Enter Engineer, Employee, or Finished List
+
+const questions = async () => {
+    const answers = await inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "Please insert name.",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Please insert ID number.",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Please insert E-Mail address.",
+            name: "email"
+        },
+        {
+            type: "list",
+            message: "Please select role.",
+            name: ["Engineer", "Intern", "Manager"]
+        }
+    ])
+}
+
 // "Would you like to enter a new employee?"(yes/no)
 //If yes - Go to list. - List shows Employee / Engineer / Intern / Manager 
 // From the selected agent follow along with prompts to enter all of the ID information. 
